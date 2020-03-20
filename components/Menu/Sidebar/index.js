3200c8533212b9391f4a31d5dff4b4a132de1b43
bkensey <bkensey@gmail.com>
2020-03-20 19:08:48 -0400

    Render user data in small screen menu as well as full screen menu

--- components/Menu/Sidebar/index.js
+++ components/Menu/Sidebar/index.js
@@ -123,25 +123,35 @@ const Sidebar = ({ isMenuOpen, setIsMenuOpen }) => {
             </nav>
           </div>
           <div className="flex flex-shrink-0 p-4 bg-gray-700">
-            <a href="#" className="flex-shrink-0 block group">
-              <div className="flex items-center">
-                <div>
-                  <img
-                    className="inline-block w-10 h-10 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
+            {!loading &&
+              (user ? (
+                <div className="flex-shrink-0 block group">
+                  <div className="flex items-center">
+                    <a href="/profile">
+                      <img
+                        className="inline-block w-10 h-10 rounded-full"
+                        src={user.picture}
+                        alt=""
+                      />
+                    </a>
+                    <div className="ml-3">
+                      <p className="text-base font-medium leading-6 text-white">
+                        {user.name}
+                      </p>
+                      <a
+                        href="/api/logout"
+                        className="text-sm font-medium leading-5 text-gray-400 transition duration-150 ease-in-out group-hover:text-gray-300"
+                      >
+                        Logout
+                      </a>
+                    </div>
+                  </div>
                 </div>
-                <div className="ml-3">
-                  <p className="text-base font-medium leading-6 text-white">
-                    Tom Cook
-                  </p>
-                  <p className="text-sm font-medium leading-5 text-gray-400 transition duration-150 ease-in-out group-hover:text-gray-300">
-                    View profile
-                  </p>
-                </div>
-              </div>
-            </a>
+              ) : (
+                <p className="text-sm font-medium leading-5 text-white">
+                  <a href="/api/login">Login</a>
+                </p>
+              ))}
           </div>
         </div>
       </div>
