bf513444b84bdaaf524f0dc047897de148da2c3e
bkensey <bkensey@gmail.com>
2020-03-21 23:25:23 -0400

    Define colors from Mixxer brand guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        "mixxer-shell-white": "#F0EBE0",
+        "mixxer-dark-gray": "#343434",
+        "mixxer-caviar": "#313031",
+        "mixxer-cherry-red": "#EC2526",
+        "mixxer-positive-red": "#AD2C34"
+      }
+    }
   },
   variants: {},
   plugins: [
