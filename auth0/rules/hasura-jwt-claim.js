9e7c1293b60a746b0dbe94a8b1bf4fa09d23b255
bkensey <bkensey@gmail.com>
2020-07-24 23:03:11 -0400

    Add Auth0 rules for repo initializations

---
+++ auth0/rules/hasura-jwt-claim.js
@@ -0,0 +1,34 @@
+function hasuraJwtClaim(user, context, callback) {
+  const QUERY_BODY = {
+    // could get roles here
+    query: `{
+              user(where: {auth0_user_id: {_eq: "${user.user_id}"}}) {
+                  id
+                }
+            }`
+  };
+
+  request.post(
+    {
+      headers: {
+        'content-type': 'application/json',
+        'x-hasura-admin-secret': configuration.HASURA_GRAPHQL_ADMIN_SECRET
+      },
+      url: configuration.HASURA_GRAPHQL_URL,
+      body: JSON.stringify(QUERY_BODY)
+    },
+    function (error, response, body) {
+      if (error) console.log('error', error);
+      else {
+        console.log('body', body);
+        const namespace = 'https://hasura.io/jwt/claims';
+        context.idToken[namespace] = {
+          'x-hasura-default-role': 'user',
+          'x-hasura-allowed-roles': ['user'], // can add custom logic to decide allowed roles
+          'x-hasura-user-id': JSON.parse(body).data.user[0].id
+        };
+        callback(null, user, context);
+      }
+    }
+  );
+}
---
+++ auth0/rules/upsert-user.js
@@ -0,0 +1,25 @@
+function upsertUser(user, context, callback) {
+  const userId = user.user_id;
+
+  const QUERY_BODY = {
+    query:
+      'mutation ($userId: bpchar!) {  insert_user(objects: { auth0_user_id: $userId}, on_conflict: {constraint: user_auth0_user_id_key, update_columns: []}) {    affected_rows  }}',
+    variables: { userId: userId }
+  };
+
+  request.post(
+    {
+      headers: {
+        'content-type': 'application/json',
+        'x-hasura-admin-secret': configuration.HASURA_GRAPHQL_ADMIN_SECRET
+      },
+      url: configuration.HASURA_GRAPHQL_URL,
+      body: JSON.stringify(QUERY_BODY)
+    },
+    function (error, response, body) {
+      if (error) console.log('error', error);
+      else console.log(body);
+      callback(null, user, context);
+    }
+  );
+}
